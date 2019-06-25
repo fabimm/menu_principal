4a58ccc628fa490accdc6783c3212df88cc4c118
fabimm <35699415+fabimm@users.noreply.github.com>
2019-06-25 00:00:55 -0600

    Creación de menú principal aún sin referencias a las páginas que lleva cada opción de menú

---
+++ resources/views/menu_principal.blade.php
@@ -0,0 +1,88 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+    <title>Menú principal</title>
+</head>
+<body>
+    <div class="container-fluid" style="background-color: #438eb9; color: white; margin-bottom: 10px; margin-top: 40px;">
+        <h2 align="center">DEPARTAMENTO DE INGENIERÍA DE PROCESOS Y CIENCIAS AMBIENTALES</h2>
+    </div>
+    <div class="container-fluid" style="background-color: #e1f2f7; padding-top: 50px; padding-bottom: 50px;">
+        <div class="container-fluid">
+            <div class="row justify-content-md-center">
+                <div class="col-3">
+                    <a href="articulos">
+                        <img src="images/gestion-art.svg" width="100" height="100" class="rounded mx-auto d-block" alt="gestion de articulos">
+                    </a>
+                </div>
+                <div class="col-3">
+                    <a href="entrada">
+                        <img src="images/entradas.svg" width="100" height="100" class="rounded mx-auto d-block" alt="gestion de articulos">
+                    </a>
+                </div>
+                <div class="col-3">
+                    <a href="salidas">
+                        <img src="images/salidas.svg" width="100" height="100" class="rounded mx-auto d-block" alt="gestion de articulos">
+                    </a>
+                </div>
+            </div>
+            <div class="row justify-content-md-center">
+                <div class="col-3">
+                    <a href="articulos">
+                        <p align="center">Gestión de artículos</p>
+                    </a>
+                </div>
+                <div class="col-3">
+                    <a href="entrada">
+                        <p align="center">Gestión de entradas</p>
+                    </a>
+                </div>
+                <div class="col-3">
+                    <a href="salidas">
+                        <p align="center">Gestión de salidas</p>
+                    </a>
+                </div>
+            </div>
+            <div class="row justify-content-md-center">
+                <div class="col-3">
+                    <a href="catalogos">
+                        <img src="images/catalogos.svg" width="100" height="100" class="rounded mx-auto d-block" alt="gestion de articulos">
+                    </a>
+                </div>
+                <div class="col-3">
+                    <a href="ajuste">
+                        <img src="images/ajuste.svg" width="100" height="100" class="rounded mx-auto d-block" alt="gestion de articulos">
+                    </a>
+                </div>
+                <div class="col-3">
+                    <a href="reportes">
+                        <img src="images/reportes.svg" width="100" height="100" class="rounded mx-auto d-block" alt="gestion de articulos">
+                    </a>
+                </div>
+            </div>
+            <div class="row justify-content-md-center">
+                <div class="col-3">
+                    <a href="catalogos">
+                        <p align="center">Mantenimiento de catálogos</p>
+                    </a>
+                </div>
+                <div class="col-3">
+                    <a href="ajuste">
+                        <p align="center">Ajuste de inventario</p>
+                    </a>
+                </div>
+                <div class="col-3">
+                    <a href="reportes">
+                        <p align="center">Generación de reportes</p>
+                    </a>
+                </div>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
